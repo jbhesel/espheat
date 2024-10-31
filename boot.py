55a9853e0b26e617f281d5e7b483ed2211b0ed07
jbhesel <extra@heselhof.de>
2024-10-31 13:39:20 +0100

    Create boot.py

---
+++ boot.py
@@ -0,0 +1,40 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+# boot.py for Esp32
+import machine
+import time
+import qlib
+import qtalk
+import qsens
+
+starttime = time.time()
+qversion = 'OA24-1'
+
+def runTime():
+    # returns elapsed time in seconds
+    return time.time() - starttime
+    
+def work(threshold=1.0):
+    qsens.measure()
+    if qsens.hasChanges(threshold):
+        qsens.setChanges()
+        url = qini.URL + "?v="+qversion+"&machine="+qlib.machineId()
+        response=qtalk.put_https(url,{"data":qsens.jsonData()})
+        if response:
+            print('response:',response)    
+
+def main():
+    qtalk.connect_wifi(qini.WIFI_SSID, qini.WIFI_PASSWORD)
+    qsens.buildSensorArray()
+    print(qsens.jsonData())
+    starttime = time.time()
+    timeout = 3600*24
+    stepS = 15
+    while (time.time() - starttime) < timeout:
+        print(runTime(), qlib.YmdHms())
+        work(1.0)
+        # print('Datenbestand')
+        # qsens.jsonLine()
+        time.sleep(stepS)
+    
+if __name__ == '__main__':
+    main()
